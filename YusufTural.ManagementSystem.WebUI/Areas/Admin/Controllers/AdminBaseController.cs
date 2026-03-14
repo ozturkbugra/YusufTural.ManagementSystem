19d538c461603bf7850c7a52d91f1421c65d88af
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-03-14 18:28:39 +0300

    feat(webui): setup Admin Area infrastructure, routing and AdminBaseController

---
+++ YusufTural.ManagementSystem.WebUI/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YusufTural.ManagementSystem.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminBaseController : Controller
+    {
+        public void CreateMessage(string message, string type = "error")
+        {
+            TempData["AdminMessage"] = message;
+            TempData["AdminMessageType"] = type;
+        }
+    }
+}
