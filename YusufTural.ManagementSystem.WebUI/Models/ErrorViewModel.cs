ea90a91f5797c3a905c58154f928b2745ec7d77e
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-03-14 15:26:06 +0300

    Add project files.

---
+++ YusufTural.ManagementSystem.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YusufTural.ManagementSystem.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
