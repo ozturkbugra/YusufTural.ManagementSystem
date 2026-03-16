c1baa3e0bb41c909a34c89498c3a69bb7d7df6ca
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-03-16 18:20:53 +0300

    style(footer): add designer credit link to bugraozturk.com.tr

--- YusufTural.ManagementSystem.WebUI/ViewComponents/SiteHeaderViewComponent.cs
+++ YusufTural.ManagementSystem.WebUI/ViewComponents/SiteHeaderViewComponent.cs
@@ -7,11 +7,19 @@ namespace YusufTural.ManagementSystem.WebUI.ViewComponents
     {
         private readonly ISiteInformationService _siteInformationService;
         private readonly IServiceService _serviceService;
+        private readonly IAboutUsService _aboutService; // Eklendi
+        private readonly IFaqService _faqService;     // Eklendi
 
-        public SiteHeaderViewComponent(ISiteInformationService siteInformationService, IServiceService serviceService)
+        public SiteHeaderViewComponent(
+            ISiteInformationService siteInformationService,
+            IServiceService serviceService,
+            IAboutUsService aboutService,
+            IFaqService faqService)
         {
             _siteInformationService = siteInformationService;
             _serviceService = serviceService;
+            _aboutService = aboutService;
+            _faqService = faqService;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
@@ -19,7 +27,11 @@ namespace YusufTural.ManagementSystem.WebUI.ViewComponents
             var siteInfo = (await _siteInformationService.TGetListAsync()).FirstOrDefault();
             var services = await _serviceService.TGetListAsync();
 
+            // Veri var mı kontrolü yapıyoruz aga
+            ViewBag.HasAbout = (await _aboutService.TGetListAsync()).Any();
+            ViewBag.HasFaq = (await _faqService.TGetListAsync()).Any();
             ViewBag.Services = services;
+
             return View(siteInfo);
         }
     }
